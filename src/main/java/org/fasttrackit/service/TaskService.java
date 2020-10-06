221491ea65d9b11f46e3b97b7554b9a3950c8baa
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-06 15:48:02 +0300

    added task service

---
+++ src/main/java/org/fasttrackit/service/TaskService.java
@@ -0,0 +1,41 @@
+package org.fasttrackit.service;
+
+
+import org.fasttrackit.domain.Task;
+import org.fasttrackit.persistance.TaskRepository;
+import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TaskService {
+
+    private TaskRepository taskRepository = new TaskRepository();
+
+
+public void createTask(CreateTaskRequest request) throws SQLException {
+
+    System.out.println("Creating task:"  + request);
+
+    taskRepository.createTask(request);
+
+}
+
+public List<Task> getTasks() throws SQLException {
+    System.out.println("Retreiving tasks.");
+    return taskRepository.getTasks();
+}
+
+
+public void updateTask( long id, UpdateTaskRequest request) throws SQLException {
+    System.out.println("Updating task: " + id + ": " + request);
+
+    taskRepository.updateTask(id, request);
+}
+    public void deleteTask(long id) throws SQLException {
+        System.out.println("Deleting task " + id);
+        taskRepository.deleteTask(id);
+
+}
+}
